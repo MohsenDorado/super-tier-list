8bd7b55173f8435a3fa148e643ad91a95ea8c776
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-08-23 14:28:15 +0330

    fix vercel

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,31 +1,15 @@
-"use client"
-import React from 'react'
-import { useRef, useEffect } from 'react';
-import { useRefStore } from '@/store/useRefStore';
-import Hero from '@/components/home/Hero';
-import Features from '@/components/home/Features';
-
+"use server";
+import React from "react";
+import Hero from "@/components/home/Hero";
+import Features from "@/components/home/Features";
 
 const ContactPage = () => {
-  const divRef = useRef<HTMLDivElement | null>(null);
-  const setRef = useRefStore((state) => state.setRef);
-
-  
-  useEffect(() => {
-    if (divRef.current) {
-      setRef(divRef);
-    }
-  }, [divRef, setRef]);
-
-
   return (
-    <div
-    className='h-[200vh] relative'>
-      <Hero/>
-      <Features/>
-      
+    <div className="h-[200vh] relative">
+      <Hero />
+      <Features />
     </div>
-  )
-}
+  );
+};
 
-export default ContactPage
+export default ContactPage;
