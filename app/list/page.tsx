b06faa4bf66aef3b9407062ee74e7534645796c9
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-08-29 14:35:05 +0330

    The dates Done!

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 import React from "react";
 import List from "@/components/list/List";
+import AddList from "@/components/list/AddList";
 
 const ListPage = () => {
 
@@ -11,6 +12,7 @@ const ListPage = () => {
   return (
 
     <div className="mt-[150px] mb-[150px] ">
+      <AddList/>
       <List/>
     </div>
  
--- components/list/List.tsx
+++ components/list/List.tsx
@@ -3,10 +3,12 @@
 import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@/public/Profile_avatar_placeholder_large.png";
 import jalaali from "jalaali-js";
 import { convertToPersianNumerals } from "@/utils/convertNumbers";
+import Link from "next/link";
+import { CalendarDays, Clock } from "lucide-react";
 
 export default function List() {
   return (
@@ -18,12 +20,24 @@ export default function List() {
   );
 }
 function ListCard() {
+  const [searched, setSearched] = useState("");
   const { isLoading, error, data } = useQuery({
     queryKey: ["listData"],
     queryFn: () => fetch("api/list").then((res) => res.json()),
   });
-  function getPersianDate(gregorianDate: Date): string {
-    const { jy, jm, jd } = jalaali.toJalaali(gregorianDate);
+  function getPerdianTime(gregorianDate: Date): string {
+    const hours = gregorianDate.getHours();
+    const minutes = gregorianDate.getMinutes();
+    const seconds = gregorianDate.getSeconds();
+    const time = `${convertToPersianNumerals(`${hours} : ${minutes}`)}`;
+    return time;
+  }
+  function getPersianDate(gregorianDate: Date, isOneMonthEnd:boolean): string {
+    let modifiedDate = new Date(gregorianDate);
+
+    modifiedDate.setDate(modifiedDate.getDate() + 90);
+    console.log(gregorianDate);
+    const { jy, jm, jd } = jalaali.toJalaali(isOneMonthEnd?modifiedDate:gregorianDate);
     const persianDate = `${convertToPersianNumerals(
       jy.toString()
     )} / ${convertToPersianNumerals(
@@ -73,50 +87,50 @@ function ListCard() {
   }
   return (
     <div className="grid grid-cols-1 lg:mx-[17%]  md:grid-cols-1 xl:grid-cols-2 gap-4 px-[50px] max-sm:px-[25px] dark:text-white ">
-      {isLoading && (
-         Array.from({ length: 5 }).map((_, index) =>(
-
-         
-        
-       
-        <div
-
-        key={index}
-          className="rounded-xl shadow-md bg-slate-50 dark:bg-slate-800 transition-all duration-500 flex
+      {/* <div className="w-full">
+    <input onChange={(e)=>setSearched(e.target.value)} value={searched} type="text" className="p-2 border rounded-xl w-full font-Yekan " placeholder="جستجو" />
+   </div> */}
+      {isLoading &&
+        Array.from({ length: 5 }).map((_, index) => (
+          <div
+            key={index}
+            className="rounded-xl shadow-md bg-slate-50 dark:bg-slate-800 transition-all duration-500 flex
        items-center justify-center flex-col p-4 my-3 font-vazir w-full"
-        >
-          <div className=" flex items-center justify-between  w-full">
-            <p className="text-right right-0 mx-3 w-full md:text-xl font-bold h-[50%] animate-pulse rounded-xl bg-slate-50   "></p>
-            <div className="relative w-16 h-16 max-sm:w-10 max-sm:h-10 rounded-full overflow-hidden flex-shrink-0   ">
-              <div
-                className={`absolute w-full h-full animate-pulse bg-slate-200  `}
-              ></div>
+          >
+            <div className=" flex items-center justify-between  w-full">
+              <p className="text-right right-0 mx-3 w-full md:text-xl font-bold h-[50%] animate-pulse rounded-xl bg-slate-50   "></p>
+              <div className="relative w-16 h-16 max-sm:w-10 max-sm:h-10 rounded-full overflow-hidden flex-shrink-0   ">
+                <div
+                  className={`absolute w-full h-full animate-pulse bg-slate-200  `}
+                ></div>
+              </div>
             </div>
-          </div>
-          {/* //!بدهی */}
-          <div className="flex items-center justify-end w-full ">
-            <div className="font-vazir  text-xl my-10  w-full h-full">
-              <div className="flex items-center justify-end gap-5 max-sm:text-lg w-full h-full ">
-                <p className="w-[200px] h-5 bg-slate-50 animate-pulse rounded-xl right-0"></p>
-                <p></p>
+            {/* //!بدهی */}
+            <div className="flex items-center justify-end w-full ">
+              <div className="font-vazir  text-xl my-10  w-full h-full">
+                <div className="flex items-center justify-end gap-5 max-sm:text-lg w-full h-full ">
+                  <p className="w-[200px] h-5 bg-slate-50 animate-pulse rounded-xl right-0"></p>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+            {/* //! : تاریخ تشکیل */}
+            <div className="flex items-center justify-end w-full ">
+              <div className="font-vazir  text-xl   w-full h-full">
+                <div className="flex items-center justify-end gap-5 max-sm:text-lg w-full h-full ">
+                  <p className="w-[200px] h-5 bg-slate-50 animate-pulse rounded-xl right-0"></p>
+                  <p></p>
+                </div>
               </div>
             </div>
           </div>
-          {/* //! : تاریخ تشکیل */}
-          <div className="flex items-center justify-end w-full ">
-            <div className="font-vazir  text-xl my-10  w-full h-full">
-              <div className="flex items-center justify-end gap-5 max-sm:text-lg w-full h-full ">
-                <p className="w-[200px] h-5 bg-slate-50 animate-pulse rounded-xl right-0"></p>
-                <p></p>
-              </div>
-            </div>
-          </div>
-        </div>
-     )) )}
+        ))}
+      {/* //!The data map..................... */}
       {data?.map((item: any) => (
-        <div
+        <Link
+          href={`list/${item.id}`}
           key={item.id}
-          className="rounded-xl shadow-md bg-slate-50 dark:bg-slate-800 transition-all duration-500 flex
+          className="rounded-xl shadow-md bg-slate-50 hover:brightness-90 dark:bg-slate-800 transition-all duration-100 flex
          items-center justify-center flex-col p-4 my-3 font-vazir w-full"
         >
           {/* //!Top of card */}
@@ -146,34 +160,63 @@ function ListCard() {
               </div>
             </div>
           </div>
-          {/* //! : تاریخ تشکیل */}
-          <div className="flex items-center justify-end w-full">
-            <div className="font-vazir  text-xl">
-              <div className="flex items-center justify-center gap-5 max-sm:text-sm">
-                <p>{getPersianDate(createFormat(item.createdAt))}</p>
-                <p>: تاریخ تشکیل</p>
+          {/* //! : تاریخ  */}
+          <div className="flex items-center justify-between w-full">
+            {/* //!اتمام مهلت */}
+          <div className="font-vazir  text-xl  w-full flex items-center justify-center ">
+              <div className="flex items-center justify-center flex-col gap-2 max-sm:text-sm">
+                <p className=" w-full text-right text-lg"> : اتمام مهلت</p>
+                <div className="pr-5 flex flex-col gap-2 ">
+
+                <div className=" text-lg text-slate-600 w-full text-right items-center justify-end gap-1 flex  ">
+                  <p>
+
+                  {getPersianDate(createFormat(item.createdAt),true)}
+                  </p>
+                  <CalendarDays className="right-0 w-6 h-6" /> 
+                </div>
+                <div className=" text-lg text-slate-600 w-full text-right items-center justify-end gap-1 flex  ">
+                  <p>
+
+                  {getPerdianTime(createFormat(item.createdAt))}
+                  </p>
+                  <Clock className="right-0 w-6 h-6" /> 
+                </div>
+                </div>
               </div>
             </div>
+            {/* //!خط.............. */}
+            <div className="w-[1px] h-full bg-slate-400 rounded-full">
+              
+            </div>
+            {/* //!تاریخ تشکیل */}
+            <div className="font-vazir  text-xl  w-full  flex items-center justify-center">
+              <div className="flex items-center justify-center flex-col gap-2 max-sm:text-sm">
+                <p className=" w-full text-right text-lg">: تاریخ تشکیل</p>
+                <div className="pr-5 flex flex-col gap-2">
+
+                <div className=" text-lg text-slate-600 w-full text-right items-center justify-end gap-1 flex  ">
+                  <p>
+
+                  {getPersianDate(createFormat(item.createdAt),false)}
+                  </p>
+                  <CalendarDays className="right-0 w-6 h-6" /> 
+                </div>
+                <div className=" text-lg text-slate-600 w-full text-right items-center justify-end gap-1 flex  ">
+                  <p>
+
+                  {getPerdianTime(createFormat(item.createdAt))}
+                  </p>
+                  <Clock className="right-0 w-6 h-6" /> 
+                </div>
+                </div>
+
+              </div>
+            </div>
+           
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
 }
-
-// <div className="flex items-center justify-end w-full ">
-//             <div className=" flex items-center justify-between  w-full">
-//               <p className="text-right mx-3 w-full md:text-xl font-bold right-0  max-sm:translate-x-[60px] sm:translate-x-[60px] md:translate-x-[60px]  ">{item.person}</p>
-//               <div className="relative w-[35%]  flex items-center justify-end ">
-//                 <div
-//                   style={{ backgroundColor: numberToRandomColor(item.id) }}
-//                   className={`absolute rounded-full shadow-xl   w-16 h-16 max-sm:w-10 max-sm:h-10`}
-//                 ></div>
-//                 <Image
-//                   src={Avatar}
-//                   alt="user-image"
-//                   className="rounded-full w-16 h-16 max-sm:w-10 max-sm:h-10 opacity-80 border-2 border-black"
-//                 />
-//               </div>
-//             </div>
-//           </div>
